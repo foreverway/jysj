8ad1895d45046d6d141c6c9edb80736b4078c378
zhaoxiforever <347413046@qq.com>
2019-10-02 01:48:23 +0800

    没什么

--- hputai/src/App.vue
+++ hputai/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-     
-    <router-view/>
+     <router-view/>
+    <!-- <router-view></router-view> -->
   </div> 
 </template>
 
@@ -15,6 +15,7 @@ export default {
 /* 表格颜色 */
   .el-table .warning-row {
     background: oldlace;
+     /* background: oldlace; */
   }
 
   .el-table .success-row {
